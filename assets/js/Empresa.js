776a00a606ddda394c70fb6a94271f8eaa3dd1cd
marta <rulex12@gmail.com>
2018-01-29 12:49:20 +0100

    Empresa closure

--- assets/js/Empresa.js
+++ assets/js/Empresa.js
@@ -1,4 +1,35 @@
-var Empresa = function Empresa(nombre, apellido, email, contraseña, confirm_contraseña, empresa, nif, telfn, stack) {
+//CLASE EMPRESA
+//Versión 2 (closure)
+var Empresa = (function(){
+    function Empresa(nombre, apellido, email, contraseña, confirm_contraseña, empresa, nif, telfn, stack) {
+        nombre = this.nombre;
+        apellido = this.apellido;
+        email = this.email;
+        contraseña = this.contraseña;
+        confirmación_contraseña = this.confirm_contraseña;
+        empresa = this.empresa;
+        nif = this.nif;
+        telefono = this.telfn;
+        stack_Tecnológico = this.stack;
+    };
+    Empresa.prototype = {
+        registrarEmpresa: function (nombre, apellido, email, contraseña, confirm_contraseña, empresa, nif, telfn, stack) {
+            nombre = this.nombre;
+            apellido = this.apellido;
+            email = this.email;
+            contraseña = this.contraseña;
+            confirmación_contraseña = this.confirm_contraseña;
+            empresa = this.empresa;
+            nif = this.nif;
+            telefono = this.telfn;
+            stack_Tecnológico = this.stack;
+        }
+    }
+    return Empresa;
+})();
+
+//Versión 1
+/*var Empresa = function Empresa(nombre, apellido, email, contraseña, confirm_contraseña, empresa, nif, telfn, stack) {
     nombre = this.nombre;
     apellido = this.apellido;
     email = this.email;
@@ -19,4 +50,4 @@ Empresa.prototype.registrarEmpresa = function (nombre, apellido, email, contrase
     nif = this.nif;
     telefono = this.telfn;
     stack_Tecnológico = this.stack;
-};
+};*/

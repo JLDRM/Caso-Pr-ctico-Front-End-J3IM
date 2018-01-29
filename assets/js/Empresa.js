90a86e3402469ac6a247dab48cf17b5ad38ccb3a
marta <rulex12@gmail.com>
2018-01-29 11:58:49 +0100

    empresa

---
+++ assets/js/Empresa.js
@@ -0,0 +1,22 @@
+var Empresa = function Empresa(nombre, apellido, email, contraseña, confirm_contraseña, empresa, nif, telfn, stack) {
+    nombre = this.nombre;
+    apellido = this.apellido;
+    email = this.email;
+    contraseña = this.contraseña;
+    confirmación_contraseña = this.confirm_contraseña;
+    empresa = this.empresa;
+    nif = this.nif;
+    telefono = this.telfn;
+    stack_Tecnológico = this.stack;
+};
+Empresa.prototype.registrarEmpresa = function (nombre, apellido, email, contraseña, confirm_contraseña, empresa, nif, telfn, stack) {
+    nombre = this.nombre;
+    apellido = this.apellido;
+    email = this.email;
+    contraseña = this.contraseña;
+    confirmación_contraseña = this.confirm_contraseña;
+    empresa = this.empresa;
+    nif = this.nif;
+    telefono = this.telfn;
+    stack_Tecnológico = this.stack;
+};

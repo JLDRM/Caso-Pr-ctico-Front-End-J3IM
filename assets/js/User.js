46ca9c8e30215d49cb220778225771f1e5ea6c34
GitPusher <kamalonu@amail.club>
2018-01-29 14:57:49 +0100

    Auto stash before merge of "master" and "origin/master"

--- assets/js/User.js
+++ assets/js/User.js
@@ -1,6 +1,5 @@
 // PROTOTIPO USUARIO GEEK
 
-// Version 2 con closure
 var Usuario = (function () {
     // Constructor
     function User(nom, cognom, foto, desc, habP, hab, port, exp, form, correu, pass, tel, hob) {
@@ -21,31 +20,3 @@ var Usuario = (function () {
 
     return User;
 })();
-
-// Version 1 sin closure
-/*
-var Usuario = function Usuario(nom, cognom, foto, desc, habP, hab, port, exp, form, correu, pass, tel, hob) {
-    this.nombre = nom;
-    this.apellidos = cognom;
-    this.fotoPerfil = foto;
-    this.descripcion = desc;
-    this.habilidadesPrincipales = habP;
-    this.habilidades = hab;
-    this.portafolio = port;
-    this.experiencia = exp;
-    this.formacion = form;
-    this.email = correu;
-    this.password = pass;
-    this.telefono = tel;
-    this.hobby = hob;
-};
-
-Usuario.prototype.registrarUsuario = function (nom, cognom, correu, pass, tel, hab, hob) {
-    this.nombre = nom;
-    this.apellidos = cognom;
-    this.email = correu;
-    this.password = pass;
-    this.telefono = tel;
-    this.habilidades = hab;
-    this.hobby = hob;
-}*/
